232b057c95e351161ff4b6c0ecb40a92c7807636
Arya <arya@Aryas-MacBook-Pro.local>
2025-03-04 19:15:23 +0530

    Awards Images

--- src/components/AwardTypeSection.jsx
+++ src/components/AwardTypeSection.jsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import Winner from "@/../public/images/winner-of-the-contest.webp";
-import DesignAward from "@/../public/images/design-award-winner.webp";
-import Expert from "@/../public/images/expert-img.webp";
-import StarAgency from "@/../public/images/5-star-agency.webp";
-import Professionals from "@/../public/images/a1-professionals.webp";
+import Winner from "@/../public/images/awards/CSS-Winner.webp";
+import Awwwards from "@/../public/images/awards/Awwwards.webp";
+import CSSDA from "@/../public/images/awards/CSSDA.webp";
+import StarAgency from "@/../public/images/awards/5-Star-Agency.webp";
+import Professionals from "@/../public/images/awards/A1-Professional.webp";
 
 const AwardType = () => {
 	return (
@@ -12,9 +12,9 @@ const AwardType = () => {
 				<div className="container">
 					<div className="type_section">
 						<ul>
-							<li><Image src={Winner} alt="Winner" quality={100}/></li>
-							<li><Image src={DesignAward} alt="DesignAward" quality={100}/></li>
-							<li><Image src={Expert} alt="Expert" quality={100}/></li>
+							<li><Image src={Winner} alt="CSSWinner" quality={100}/></li>
+							<li><Image src={Awwwards} alt="Awwwards" quality={100}/></li>
+							<li><Image src={CSSDA} alt="CSS Design" quality={100}/></li>
 							<li><Image src={StarAgency} alt="StarAgency" quality={100}/></li>
 							<li><Image src={Professionals} alt="Professionals" quality={100}/></li>
 						</ul>
